6bb8b9dae191b7ba813ec0973fcfbe451f2b3421
Amanda Schneider Bassani <69223606+amandasbassani@users.noreply.github.com>
2023-06-20 14:18:27 -0300

    removing sigmoid option

--- testingData.py
+++ testingData.py
@@ -10,7 +10,6 @@ with open(f'./datafiles/{dbname}/info.json', 'r') as f:
     info = json.load(f)
 
 trName = 'model_otim1'
-classification = info['classification']
 
 with open(f'./datafiles/{dbname}/models/test{trName}.pkl', 'rb') as f:
     test = pickle.load(f)
@@ -22,11 +21,8 @@ model = tf.keras.models.load_model(f'./datafiles/{dbname}/models/model{trName}.h
 
 predict_x=model.predict(X_test)
 
-y_pred = None
-if classification == "sigmoid":
-    y_pred = np.zeros(predict_x.shape)
-    y_pred[predict_x>=0.5] = 1
-elif classification == "softmax":
-    y_pred = np.around(predict_x)
+
+
+y_pred = np.around(predict_x)
 
 print("Test Accuracy:", np.mean(y_pred==y_test.astype(float)))
